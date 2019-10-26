66fd3e8f0a5b0ea9f119921b51f9ea8142a581aa
pawii <pawii@yandex.ru>
2019-10-26 09:01:25 +0300

    1,2 labs

---
+++ 3_UnitTesting/3_UnitTesting/Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_UnitTesting
+{
+    public class Triangle
+    {
+        public static bool IsValid(int sideA, int sideB, int sideC)
+        {
+            return (sideA < sideB + sideC)
+                && (sideB < sideA + sideC)
+                && (sideC < sideA + sideB);
+        }
+    }
+}
